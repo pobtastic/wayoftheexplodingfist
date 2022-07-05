7a9f3d8a223d1720f3f8c6be78f02608646fc4e2
Paul Maddern <paul@arcadegeek.co.uk>
2022-07-05 14:52:28 +0000

    Updates 05072022.

--- Makefile
+++ Makefile
@@ -1,30 +1,5 @@
-BUILDDIR = build
-
-OPTIONS  = -d build/html -t
-
-OPTIONS += $(foreach theme,$(THEMES),-T $(theme))
-OPTIONS += $(HTML_OPTS)
-
-.PHONY: usage clean wotef
-usage:
-	@echo "Targets:"
-	@echo "  usage      show this help"
-	@echo "  wotef      build The Way Of The Exploding Fist disassembly"
-	@echo ""
-	@echo "Variables:"
-	@echo "  THEMES     CSS theme(s) to use"
-	@echo "  HTML_OPTS  options passed to skool2html.py"
-
-.PHONY: clean
-clean:
-	-rm -rf $(BUILDDIR)/*
-
-.PHONY: wotef
-wotef:
-	if [ ! -f WayOfTheExplodingFistThe.z80 ]; then tap2sna.py @wotef.t2s; fi
-	sna2skool.py -H -c sources/wotef.ctl WayOfTheExplodingFistThe.z80 > sources/wotef.skool
-	skool2html.py $(OPTIONS) -D -c Config/GameDir=wotef/dec -c Config/InitModule=sources:bases sources/wotef.skool sources/wotef.ref
-	skool2html.py $(OPTIONS) -H -c Config/GameDir=wotef/hex -c Config/InitModule=sources:bases sources/wotef.skool sources/wotef.ref
-
-all : wotef
-.PHONY : all
+MK = $(SKOOLKIT_HOME)/tools/disassembly.mk
+ifeq ($(wildcard $(MK)),)
+	$(error $(MK): file not found)
+endif
+include $(MK)
--- utils/skrunner.py
+++ utils/skrunner.py
@@ -6,6 +6,7 @@ WAYOFTHEEXPLODINGFIST_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__f
 WOTEF_SKOOL = '{}/sources/wotef.skool'.format(WAYOFTHEEXPLODINGFIST_HOME)
 
 SKOOLKIT_HOME = os.environ.get('SKOOLKIT_HOME')
+SKOOLKIT_TOOLS = "{}/tools".format(SKOOLKIT_HOME)
 if SKOOLKIT_HOME:
     if not os.path.isdir(SKOOLKIT_HOME):
         sys.stderr.write('SKOOLKIT_HOME={}: directory not found\n'.format(SKOOLKIT_HOME))
@@ -25,9 +26,9 @@ def run_skool2asm():
     skool2asm.main(sys.argv[1:] + [WOTEF_SKOOL])
 
 def run_skool2html():
-    options = '-c Config/InitModule=sources:bases -d {}/build/html'.format(WAYOFTHEEXPLODINGFIST_HOME)
+    options = "-c Config/InitModule={}:publish -d {}/build/html".format(SKOOLKIT_TOOLS, WAYOFTHEEXPLODINGFIST_HOME)
     art.tprint("The Way Of The Exploding Fist")
-    hex = '-H -c Config/GameDir=wayoftheexplodingfist/hex'
-    dec = '-D -c Config/GameDir=wayoftheexplodingfist/dec'
+    hex = '-H -c Config/GameDir=wayoftheexplodingfist --var pub=2'
+    dec = '-D -c Config/GameDir=wayoftheexplodingfist/dec --var pub=4'
     skool2html.main(options.split() + hex.split() + sys.argv[1:] + [WOTEF_SKOOL])
     skool2html.main(options.split() + dec.split() + sys.argv[1:] + [WOTEF_SKOOL])
